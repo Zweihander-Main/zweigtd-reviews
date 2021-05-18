39a8ca05dfcab61ed37d7143a324f2917bb0a227
Zweihänder <zweidev@zweihander.me>
2021-05-17 20:38:25 -0400

    feat(root): initial port from config

---
+++ Cask
@@ -0,0 +1,17 @@
+(source gnu)
+(source melpa)
+
+(package-file "zweigtd-reviews.el")
+
+(files "process-org-agenda-inbox-test.el")
+
+(depends-on "org")
+(depends-on "org-ql")
+
+(development
+ (depends-on "f")
+ (depends-on "elsa")
+ (depends-on "ecukes")
+ (depends-on "buttercup")
+ (depends-on "ert-runner")
+ (depends-on "el-mock"))
---
+++ test/zweigtd-reviews-test.el
@@ -0,0 +1,45 @@
+;;; zweigtd-reviews-test.el --- Tests for zweigtd-reviews-*-lexical-binding:t-*-
+
+;; Copyright (C) 2021, Zweihänder <zweidev@zweihander.me>
+;;
+;; Author: Zweihänder
+;; Keywords: org-mode
+;; Homepage: https://github.com/Zweihander-Main/zweigtd-reviews
+;; Version: 0.0.1
+
+;; This file is not part of GNU Emacs.
+
+;;; License:
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU Affero General Public License as published
+;; by the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU Affero General Public License for more details.
+;;
+;; You should have received a copy of the GNU Affero General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Tests for zweigtd-reviews
+
+;;; Code:
+
+(require 'buttercup)
+(require 'zweigtd-reviews)
+
+(describe "Testing"
+  (it "works."
+    (expect t :to-equal t)))
+
+;; Local Variables:
+;; coding: utf-8
+;; flycheck-disabled-checkers: 'emacs-lisp-elsa
+;; End:
+
+;;; zweigtd-reviews-test.el ends here
